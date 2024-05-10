f9e86104afa901edb06262e7c7eda0aa2089ee20
benbaho <aydinbahadirtuna@gmail.com>
2024-05-10 19:00:17 +0300

    getGoogleOAuthUrl done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import getGoogleOAuthUrl from "@/utils/getGoogleUrl";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+      <Link href={getGoogleOAuthUrl()}>Login with Google</Link>
     </main>
   );
 }
